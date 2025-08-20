e12df185d8cfd713c887f80e3a55eab7a5ff9460
Anshuman <thakuranshuman341@gmail.com>
2025-08-20 12:32:24 +0530

    folder-structure-commit

---
+++ src/app/HomeClient.jsx
@@ -0,0 +1,15 @@
+"use Client";
+
+import React from 'react'
+import Nav from './_home/Nav';
+
+const HomeClient = () => {
+  return (
+    <>
+    <Nav />
+    
+    </>
+  )
+}
+
+export default HomeClient
