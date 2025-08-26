fe713ba63cd1baf010e8edb623013701ced9dec7
Anshuman <thakuranshuman341@gmail.com>
2025-08-26 16:09:02 +0530

    smallchnages

--- src/app/_home/Car.jsx
+++ src/app/_home/Car.jsx
@@ -1,3 +1,4 @@
+
 "use client";
 import React from "react";
 import Link from "next/link";
@@ -9,7 +10,7 @@ const Car = () => {
   return (
     <section className="bg-[#f3f3f4] py-10 sm:py-16 md:py-20 lg:py-22 my-10 sm:my-14 md:my-16 lg:my-19 max-sm:py-20 ">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row justify-between items-center gap-4 max-sm:gap-0 max-sm:items-start max-sm:mb-0 max-sm:justify-items-flex-start max-md:justify-items-flex-start max-md:items-start max-md:mb-0  max-md:gap-0  ">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4 max-sm:gap-0 max-sm:items-start max-sm:mb-0 max-sm:justify-items-flex-start max-md:justify-items-flex-start max-md:items-start max-md:mb-0  max-md:gap-0">
           <div>
             <h2 className="text-[#121212] rounded-4xl w-fit text-base max-sm:text-base max-md:text-base uppercase">
               Our Car Offerings
@@ -19,23 +20,27 @@ const Car = () => {
               <span className="text-[#ff2525]">a Luxury Experience</span>
             </h1>
           </div>
+
+          
           <Link
             href="/car"
-            className=" max-md:hidden max-sm:hidden sm:hidden bg-[#ff2525] flex justify-center items-center gap-1 mx-1 rounded-lg text-white px-4 sm:px-5 md:px-6 py-2 sm:py-2.5 md:py-3 text-sm sm:text-base transition-all duration-300 hover:bg-[#ff3232] max-md:text-nowrap max-lg:text-nowrap max-sm:w-fit "
+            className="hidden lg:flex bg-[#ff2525] justify-center items-center gap-1 mx-1 rounded-lg text-white px-4 sm:px-5 md:px-6 py-2 sm:py-2.5 md:py-3 text-sm sm:text-base transition-all duration-300 hover:bg-[#ff3232]"
           >
             Explore Now{" "}
             <GoArrowUpRight className="w-4 sm:w-[18px] h-4 sm:h-[18px] rotate-0" />
           </Link>
         </div>
 
-        <ul className="flex flex-wrap w-full items-center mt-6 max-sm:flex-col max-md:gap-2 gap-4  max-md:justify-between max-xl:gap-4 max-sm:items-start  ">
+        <ul className="flex flex-wrap w-full items-center mt-6 max-sm:flex-col max-md:gap-2 gap-4 max-md:justify-between max-xl:gap-4 max-sm:items-start">
           {data.map((item, index) => (
             <Carcard item={item} index={index} key={index} />
           ))}
         </ul>
+
+       
         <Link
           href="/car"
-          className=" max-sm:hidden mt-7 md:mt-0 bg-[#ff2525] flex justify-center  items-center gap-2 rounded-lg text-white px-6 py-3 text-base font-semibold transition-all duration-300 hover:bg-[#ff3232]"
+          className="flex lg:hidden mt-7 bg-[#ff2525] justify-center items-center gap-2 rounded-lg text-white px-6 py-3 text-base font-semibold transition-all duration-300 hover:bg-[#ff3232]"
         >
           Explore Now <GoArrowUpRight className="w-[18px] h-[18px]" />
         </Link>
--- src/app/_home/Package.jsx
+++ src/app/_home/Package.jsx
@@ -1,4 +1,56 @@
 
+// "use client";
+// import { GoArrowUpRight } from "react-icons/go";
+// import React from "react";
+// import Link from "next/link";
+// import { data } from "@/store/Packagedata";
+// import Packagecard from "@/utilities/Packagecard";
+
+
+
+// const Package = () => {
+//   return (
+   
+    
+//     <div className="container mx-auto px-3 mt-28 max-sm:mt-17">
+     
+//       <div className="flex flex-col md:flex-row justify-between items-center mb-12 max-sm:items-start max-md:items-start max-md:mb-8 ">
+//         <div>
+//           <h2 className="text-[#121212] text-[16px] uppercase tracking-wide">
+//             Our Travel Packages
+//           </h2>
+//           <h1 className="text-[32px] md:text-[43px] font-black text-[#1B1917] leading-tight max-md:text-[37px]
+//           ">
+//             Discover,{" "}
+//             <span className="text-[#ff2525]">Beyond the Destination</span>
+//           </h1>
+//         </div>
+//         <Link
+//           href="/car"
+//           className=" max-sm:hidden max-lg:text-nowrap mt-6 md:mt-0 bg-[#ff2525] flex justify-center  items-center gap-2 rounded-lg text-white px-6 py-3 text-base font-semibold transition-all duration-300 hover:bg-[#ff3232]"
+//         >
+//           Explore Now <GoArrowUpRight className="w-[18px] h-[18px]" />
+//         </Link>
+//       </div>
+
+    
+//       <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 w-full">
+//         {data.map((item, index) => (
+//           <Packagecard key={index} item={item} />
+//         ))}
+//       </ul>
+//       <Link
+//           href="/car"
+//           className="  md:hidden mt-10 md:mt-0 bg-[#ff2525] flex justify-center  items-center gap-2 rounded-lg text-white px-6 py-3 text-base font-semibold transition-all duration-300 hover:bg-[#ff3232]"
+//         >
+//           Explore Now <GoArrowUpRight className="w-[18px] h-[18px]" />
+//         </Link>
+     
+//   </div>
+//   );
+// };
+
+// export default Package;
 "use client";
 import { GoArrowUpRight } from "react-icons/go";
 import React from "react";
@@ -6,47 +58,45 @@ import Link from "next/link";
 import { data } from "@/store/Packagedata";
 import Packagecard from "@/utilities/Packagecard";
 
-
-
 const Package = () => {
   return (
-   
-    
     <div className="container mx-auto px-3 mt-28 max-sm:mt-17">
-     
       <div className="flex flex-col md:flex-row justify-between items-center mb-12 max-sm:items-start max-md:items-start max-md:mb-8 ">
         <div>
           <h2 className="text-[#121212] text-[16px] uppercase tracking-wide">
             Our Travel Packages
           </h2>
-          <h1 className="text-[32px] md:text-[43px] font-black text-[#1B1917] leading-tight max-md:text-[37px]
-          ">
+          <h1
+            className="text-[32px] md:text-[43px] font-black text-[#1B1917] leading-tight max-md:text-[37px]"
+          >
             Discover,{" "}
             <span className="text-[#ff2525]">Beyond the Destination</span>
           </h1>
         </div>
+
+        {/* TOP BUTTON - visible only on lg and above */}
         <Link
           href="/car"
-          className=" max-sm:hidden max-lg:text-nowrap mt-6 md:mt-0 bg-[#ff2525] flex justify-center  items-center gap-2 rounded-lg text-white px-6 py-3 text-base font-semibold transition-all duration-300 hover:bg-[#ff3232]"
+          className="hidden lg:flex max-lg:text-nowrap mt-6 md:mt-0 bg-[#ff2525] justify-center items-center gap-2 rounded-lg text-white px-6 py-3 text-base font-semibold transition-all duration-300 hover:bg-[#ff3232]"
         >
           Explore Now <GoArrowUpRight className="w-[18px] h-[18px]" />
         </Link>
       </div>
 
-    
       <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 w-full">
         {data.map((item, index) => (
           <Packagecard key={index} item={item} />
         ))}
       </ul>
+
+      {/* BOTTOM BUTTON - visible only on sm & md */}
       <Link
-          href="/car"
-          className="  md:hidden mt-10 md:mt-0 bg-[#ff2525] flex justify-center  items-center gap-2 rounded-lg text-white px-6 py-3 text-base font-semibold transition-all duration-300 hover:bg-[#ff3232]"
-        >
-          Explore Now <GoArrowUpRight className="w-[18px] h-[18px]" />
-        </Link>
-     
-  </div>
+        href="/car"
+        className="flex lg:hidden mt-10 bg-[#ff2525] justify-center items-center gap-2 rounded-lg text-white px-6 py-3 text-base font-semibold transition-all duration-300 hover:bg-[#ff3232]"
+      >
+        Explore Now <GoArrowUpRight className="w-[18px] h-[18px]" />
+      </Link>
+    </div>
   );
 };
 
